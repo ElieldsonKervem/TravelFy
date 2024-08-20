5659fd7c671409e8556ff4053fe0b2999b985965
ElieldsonKervem <kervem.elieldson@academico.ifrn.edu.br>
2024-08-20 20:52:16 -0300

    sassadd - vueAdd -> working on header of the project

---
+++ src/components/CardMenu.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+
+    <div  class="cardManu">
+        <header class="mainHeader">
+             <div class="container">
+                <div>
+                    <h2 class="logo">TRAVELFY</h2>
+                </div>
+                <div>
+                    <ul class="menuList">
+                        <li class="menuItem">
+                            <a href="#">HOME</a>
+                        </li>
+                        <li class="menuItem">
+                            <a href="">DESTINOS</a>
+                        </li>
+                        <li class="menuItem">
+                            <a href="">PACOTES</a>
+                        </li>
+                        <li class="menuItem">
+                            <a href="">CONTATO</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <button class="btnLogin">LOGIN</button>
+                    <button class="btnLogin">SIGN UP</button>
+                </div>
+             </div>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
