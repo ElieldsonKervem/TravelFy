f277afbc46ea9285c649bbafa8ad2a5147fa1dec
ElieldsonKervem <kervem.elieldson@academico.ifrn.edu.br>
2024-08-26 20:51:11 -0300

    working on mobile and add behavior of the site

--- src/components/CardSecundary.vue
+++ src/components/CardSecundary.vue
@@ -18,72 +18,24 @@ import { ref } from 'vue'
            <div class="cardImgsIcons">
                <div class="cardIcon1">
                      <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="161" height="161" viewBox="0 0 161 161" fill="none">
-                                <g filter="url(#filter0_d_9_103)">
-                                    <circle cx="80.5" cy="76.5" r="76.5" fill="#4E00FF"/>
-                                </g>
-                                <defs>
-                                    <filter id="filter0_d_9_103" x="0" y="0" width="161" height="161" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                                    <feOffset dy="4"/>
-                                    <feGaussianBlur stdDeviation="2"/>
-                                    <feComposite in2="hardAlpha" operator="out"/>
-                                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_9_103"/>
-                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_9_103" result="shape"/>
-                                    </filter>
-                                </defs>
-                        </svg>
-                        <img src="../../public/mdi_world-plus.png" alt="planeta com simbolo +" class="iconCards3">
+                     
+                        <img src="../../public/world.svg" alt="planeta com simbolo +" class="iconCards3">
                      </div>
                       <h3>Multiplos Destinos</h3>
                       <p>Explore o mundo com a liberdade de múltiplos destinos! Combine várias cidades e países em uma única viagem.</p>
                </div>
                <div class="cardIcon2">
                      <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="161" height="161" viewBox="0 0 161 161" fill="none">
-                                <g filter="url(#filter0_d_9_103)">
-                                    <circle cx="80.5" cy="76.5" r="76.5" fill="#4E00FF"/>
-                                </g>
-                                <defs>
-                                    <filter id="filter0_d_9_103" x="0" y="0" width="161" height="161" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                                    <feOffset dy="4"/>
-                                    <feGaussianBlur stdDeviation="2"/>
-                                    <feComposite in2="hardAlpha" operator="out"/>
-                                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_9_103"/>
-                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_9_103" result="shape"/>
-                                    </filter>
-                                </defs>
-                        </svg>
-                        <img src="../../public/bi_people-fill.png" alt="icone de duas pessoas" class="iconCards2">
+                       
+                        <img src="../../public/people.svg" alt="icone de duas pessoas" class="iconCards2">
                      </div>
                       <h3>+de 5.000 clientes</h3>
                       <p>Com mais de 5.000 clientes satisfeitos, oferecemos a expertise necessária para planejar suas viagens a múltiplos destinos.</p>
                </div>
                <div class="cardIcon3">
                      <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="161" height="161" viewBox="0 0 161 161" fill="none">
-                                <g filter="url(#filter0_d_9_103)">
-                                    <circle cx="80.5" cy="76.5" r="76.5" fill="#4E00FF"/>
-                                </g>
-                                <defs>
-                                    <filter id="filter0_d_9_103" x="0" y="0" width="161" height="161" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                                    <feOffset dy="4"/>
-                                    <feGaussianBlur stdDeviation="2"/>
-                                    <feComposite in2="hardAlpha" operator="out"/>
-                                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_9_103"/>
-                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_9_103" result="shape"/>
-                                    </filter>
-                                </defs>
-                        </svg>
-                        <img src="../../public/mdi_email-certified.png" alt="icone de certificado" class="iconCards1">
+                       
+                        <img src="../../public/emailsemd.svg" alt="icone de certificado" class="iconCards1">
                      </div>
                       <h3>Certificada</h3>
                       <p>Certificada e confiável, a Travelfy atende mais de 5.000 clientes com excelência em viagens a múltiplos destinos. </p>
